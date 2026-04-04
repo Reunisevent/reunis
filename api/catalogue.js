2d215cc8ce8da1827f1b549e36f11f97cdb11049
Reunisevent <lise.pinceloup@gmail.com>
2026-04-04 08:26:34 +0200

    Update catalogue.js

--- api/catalogue.js
+++ api/catalogue.js
@@ -2,9 +2,22 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
 
   try {
-    const response = await fetch('https://httpbin.org/get');
+    const response = await fetch(
+      'https://api.notion.com/v1/databases/6d80f8aeaa1e45bab692f84f0821737d/query',
+      {
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${process.env.NOTION_TOKEN}`,
+          'Notion-Version': '2022-06-28',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ page_size: 3 })
+      }
+    );
+
     const text = await response.text();
-    res.status(200).json({ test: text });
+    res.status(200).json({ raw: text });
+
   } catch (err) {
     res.status(500).json({ erreur: err.message });
   }
