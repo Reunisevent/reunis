7f1fba1fe73b39d5f3d13fd68ea2ab79d052d973
Reunisevent <lise.pinceloup@gmail.com>
2026-04-04 08:17:47 +0200

    Update catalogue.js

--- api/catalogue.js
+++ api/catalogue.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
       method: 'POST',
       headers: {
         'Authorization': `Bearer ${process.env.NOTION_TOKEN}`,
-        'Notion-Version': '2022-06-28',
+        'Notion-Version': '2025-09-03',
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({ page_size: 10 })
