f7c0b19f64307c07c34b932a7675f008d5dcc7e7
Reunisevent <lise.pinceloup@gmail.com>
2026-04-07 08:53:19 +0200

    Update catalogue.js

--- api/catalogue.js
+++ api/catalogue.js
@@ -1,8 +1,8 @@
-import { Client } from '@notionhq/client';
+const { Client } = require('@notionhq/client');
 
 const notion = new Client({ auth: process.env.NOTION_TOKEN });
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
 
   try {
@@ -27,3 +27,39 @@ export default async function handler(req, res) {
         property: 'Sous catégorie',
         select: { equals: sous_categorie }
       });
+    }
+
+    const response = await notion.databases.query({
+      database_id: process.env.NOTION_DB_ID,
+      filter: { and: filters },
+      page_size: 100
+    });
+
+    const articles = response.results.map(page => {
+      const p = page.properties;
+      return {
+        id: page.id,
+        nom: p['Nom']?.title?.[0]?.plain_text ?? '',
+        reference: p['Référence']?.rich_text?.[0]?.plain_text ?? '',
+        categorie: p['Catégorie']?.select?.name ?? '',
+        sous_categorie: p['Sous catégorie']?.select?.name ?? '',
+        sous_sous_categorie: p['Sous-sous catégorie']?.select?.name ?? '',
+        description: p['Description']?.rich_text?.[0]?.plain_text ?? '',
+        dimensions: p['Dimensions']?.rich_text?.[0]?.plain_text ?? '',
+        couleurs: p['Couleurs']?.multi_select?.map(c => c.name) ?? [],
+        statut_stock: p['Statut stock']?.select?.name ?? '',
+        qtite_en_ligne: p['Qtité en ligne']?.number ?? 0,
+        personnalisable: p['Personnalisable']?.select?.name ?? '',
+        prix_location: p['Prix location']?.number ?? null,
+        photo: p['Photo']?.files?.[0]?.file?.url
+            ?? p['Photo']?.files?.[0]?.external?.url
+            ?? null,
+      };
+    });
+
+    res.status(200).json(articles);
+
+  } catch (err) {
+    res.status(500).json({ erreur: err.message, code: err.code });
+  }
+};
